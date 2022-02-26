d87544e6481fc4517596d33f9b270e8f39bf38c2
Tim Paine <t.paine154@gmail.com>
2022-02-26 12:31:54 -0500

    .

--- airflow_pm2/__init__.py
+++ airflow_pm2/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from ._version import __version__
-from .operator import *  # noqa: F401, F403
-from .pm2 import *  # noqa: F401, F403
+from .operator import *
+from .pm2 import *
